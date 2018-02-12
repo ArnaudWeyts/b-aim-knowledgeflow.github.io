dd5534ba0a4c3d5d474b617343b33946084fedd9
Arnaud Weyts <arnaud.weyts@gmail.com>
2018-02-12 15:24:19 +0100

    update info

--- src/teaminfo.js
+++ src/teaminfo.js
@@ -2,15 +2,19 @@ export default [
   {
     id: 1,
     name: 'Arnaud Weyts',
-    description: 'Test description',
+    description:
+      "I'm a 20 year old IT student in Ghent. Fueled by coffee in the early mornings to keep up with all the coding. I live in Flanders, Belgium and love to learn new things. My hobbies include stargazing (not just on GitHub) and swimming. I'm also a fond dog person.",
     cv: 'https://google.com',
+    email: 'arnaud.weyts@student.odisee.be',
     location: [3.6, 50.9]
   },
   {
     id: 2,
-    name: 'Arnaud Weyts',
-    description: 'Test description',
+    name: 'Dennis Neumann',
+    description:
+      "I'm from Germany and study Computer Science in Master. I have programming experiences in Java, C++, general knowledge in C and MySQL. My ECTS involvement is 20 points",
     cv: 'https://google.com',
+    email: 'dennis2.neumann@student.uni-siegen.de',
     location: [23, 58]
   },
   {
