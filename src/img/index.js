88c0bc789398c960453563a1f117c07251b66556
Arnaud Weyts <arnaud.weyts@gmail.com>
2018-02-13 12:24:21 +0100

    update to teaminfo and appropriate images

--- src/img/index.js
+++ src/img/index.js
@@ -1,9 +1,16 @@
 import boy from './boy.png';
 import girl from './girl.png';
+import team from './team.png';
 
 export default {
-  1: boy,
-  2: boy,
+  1: girl,
+  2: girl,
   3: girl,
-  4: boy
+  4: girl,
+  5: boy,
+  6: boy,
+  7: boy,
+  8: boy,
+  9: boy,
+  team: team
 };
--- src/teaminfo.js
+++ src/teaminfo.js
@@ -1,26 +1,6 @@
 export default [
   {
     id: 1,
-    name: 'Arnaud Weyts',
-    description:
-      "I'm a 20 year old IT student in Ghent. Fueled by coffee in the early mornings to keep up with all the coding. I live in Flanders, Belgium and love to learn new things. My hobbies include stargazing (not just on GitHub) and swimming. I'm also a fond dog person.",
-    ects: 15,
-    cv: 'https://google.com',
-    email: ['arnaud.weyts@student.odisee.be'],
-    location: [3.6, 50.9]
-  },
-  {
-    id: 2,
-    name: 'Dennis Neumann',
-    description:
-      "I'm from Germany and study Computer Science in Master. I have programming experiences in Java, C++, general knowledge in C and MySQL.",
-    ects: 20,
-    cv: 'https://google.com',
-    email: ['dennis2.neumann@student.uni-siegen.de'],
-    location: [8.027386, 50.91084]
-  },
-  {
-    id: 3,
     name: 'Ana Rita Lage Moreira Pereira',
     description:
       'Hello! I am Rita and I am 21 years old. I live at the North of Portugal. I have a degree in Tourism Management. At the moment, I am studying Marketing. I really like Marketing since when I was younger, principally Touristic Marketing, that\'s why I am doing this Master to complete my studies. I have done a training in  "Marketing & Sales" department at Dom Henrique in Porto Hotel, during two months. In adition, I am confortable with Internacional Marketing, Innovation and Strategic Planning.',
@@ -30,7 +10,63 @@ export default [
     location: [-8.6291, 41.1579]
   },
   {
+    id: 2,
+    name: 'Magalie Fierens',
+    description: 'Visual Design / Graphic',
+    ects: 9,
+    cv: 'https://google.com',
+    email: ['magalie.fierens@student.luca-arts.be'],
+    location: [3.7174, 51.0543]
+  },
+  {
+    id: 3,
+    name: 'Ashleigh Robertson',
+    description: 'Graphic Design',
+    ects: 10,
+    cv: 'https://google.com',
+    email: ['AROBER221@caledonian.ac.uk'],
+    location: [-4.2508, 55.8662]
+  },
+  {
     id: 4,
+    name: 'Katerina Melnikova',
+    description: 'Business in Emerging Markets',
+    ects: 5,
+    cv: 'https://google.com',
+    email: ['ekaterinam.russia@gmail.com'],
+    location: [37.6173, 55.7558]
+  },
+  {
+    id: 5,
+    name: 'Arnaud Weyts',
+    description:
+      "I'm a 20 year old IT student in Ghent. Fueled by coffee in the early mornings to keep up with all the coding. I live in Flanders, Belgium and love to learn new things. My hobbies include stargazing (not just on GitHub) and swimming. I'm also a fond dog person.",
+    ects: 15,
+    cv: 'https://google.com',
+    email: ['arnaud.weyts@student.odisee.be'],
+    location: [3.6014, 50.847]
+  },
+  {
+    id: 6,
+    name: 'Dennis Neumann',
+    description:
+      "I'm from Germany and study Computer Science in Master. I have programming experiences in Java, C++, general knowledge in C and MySQL.",
+    ects: 20,
+    cv: 'https://google.com',
+    email: ['dennis2.neumann@student.uni-siegen.de'],
+    location: [8.027386, 50.91084]
+  },
+  {
+    id: 7,
+    name: 'Jonas König',
+    description: 'Business Informatics',
+    ects: 10,
+    cv: 'https://google.com',
+    email: ['jonas.koenig94@yahoo.de'],
+    location: [8.7575, 51.7189]
+  },
+  {
+    id: 8,
     name: 'Rawand Farhad',
     description:
       'Java , C#, Oracle ,MySQL. Plus had experience using the other programming languages ,(web//PHP,JS ,etc)',
@@ -38,5 +74,14 @@ export default [
     cv: 'https://google.com',
     email: ['rawand.3115014011@uhd.edu.iq', 'rawand.farhad@gmail.com'],
     location: [45.4966966, 35.5292404]
+  },
+  {
+    id: 9,
+    name: 'Georgios Peirasmakis',
+    description: 'Informatics Engineering',
+    ects: 20,
+    cv: 'https://google.com',
+    email: ['tp4073@edu.teicrete.gr'],
+    location: [25.1442, 35.3387]
   }
 ];
