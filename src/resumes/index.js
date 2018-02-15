d830f0d12668510ca7e8e42c847f2fb328a82927
Arnaud Weyts <arnaud.weyts@gmail.com>
2018-02-15 20:08:03 +0100

    update cv's

--- src/resumes/index.js
+++ src/resumes/index.js
@@ -2,5 +2,7 @@ import rita from './rita.pdf';
 import kat from './kat.pdf';
 import arnaud from './arnaud.pdf';
 import jonas from './jonas.pdf';
+import dennis from './dennis.pdf';
+import rawand from './rawand.pdf';
 
-export default [rita, null, null, kat, arnaud, null, jonas, null, null];
+export default [rita, null, null, kat, arnaud, dennis, jonas, rawand, null];
